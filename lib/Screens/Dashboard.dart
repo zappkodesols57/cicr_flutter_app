384eb06c60a3c66cb6be9adef276b3fe1564432f
milind <milindnarethe909@gmail.com>
2022-02-01 18:22:48 +0530

    anjali

--- lib/Screens/Dashboard.dart
+++ lib/Screens/Dashboard.dart
@@ -1,6 +1,15 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key key}) : super(key: key);
+
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -8,12 +17,54 @@ class Dashboard extends StatelessWidget {
         title: Text("CICR"),
       ),
       drawer: Drawer(
-
       ),
-      body: Container(
+      body:  Container(
         height: MediaQuery.of(context).size.height,
-        alignment: Alignment.center,
-        child: Text("Hello"),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: SizedBox(
+                height: 200.0,
+                width: 350.0,
+                child: Carousel(
+                  radius: Radius.circular(20.0),
+                  borderRadius:true ,
+                  boxFit: BoxFit.cover,
+                  autoplay: true,
+                  dotSpacing: 8.0,
+                  animationCurve: Curves.fastOutSlowIn,
+                  animationDuration: Duration(milliseconds: 1000),
+                  dotSize: 5.0,
+                  dotIncreasedColor: Color(0xFF4C853E),
+                  dotBgColor: Colors.black26,
+                  dotPosition: DotPosition.bottomCenter,
+                  dotVerticalPadding: 2.0,
+                  showIndicator: true,
+                  indicatorBgPadding: 7.0,
+                  images: [
+                    AssetImage("assets/one.jpg"),
+                    AssetImage("assets/two.jpg"),
+                    AssetImage("assets/three.jpg"),
+                    AssetImage("assets/four.jpg"),
+                    AssetImage("assets/five.jpg"),
+                    AssetImage("assets/six.jpg"),
+                    AssetImage("assets/seven.jpg"),
+                    AssetImage("assets/eight.jpg"),
+                    AssetImage("assets/nine.jpg"),
+                    AssetImage("assets/ten.jpg"),
+                    AssetImage("assets/eleven.jpg"),
+                    AssetImage("assets/twelve.jpg"),
+                    AssetImage("assets/thirteen.jpg"),
+
+                  ],
+                ),
+
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
